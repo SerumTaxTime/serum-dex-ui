896fdb38f78195d9b1772d1ffc8d80c77061c4d9
Gabriel Mickey <ratxt1@gmail.com>
2021-03-01 15:38:14 -0700

    Updates

---
+++ precache-manifest.e01ce8e738f6a19d2936cc47f6d72bee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f851ebc3faa21be382d550deb084718e",
+    "url": "/serum-dex-ui/index.html"
+  },
+  {
+    "revision": "7475a781ee3c957f99f2",
+    "url": "/serum-dex-ui/static/css/2.8cf2ef39.chunk.css"
+  },
+  {
+    "revision": "04bd41247455d63d05dc",
+    "url": "/serum-dex-ui/static/css/main.a1d5c795.chunk.css"
+  },
+  {
+    "revision": "7475a781ee3c957f99f2",
+    "url": "/serum-dex-ui/static/js/2.9d69e10b.chunk.js"
+  },
+  {
+    "revision": "ed364e94a6f6b1bb953368935d752ffd",
+    "url": "/serum-dex-ui/static/js/2.9d69e10b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04bd41247455d63d05dc",
+    "url": "/serum-dex-ui/static/js/main.5630203b.chunk.js"
+  },
+  {
+    "revision": "663b1c1ae30a5616e9bc",
+    "url": "/serum-dex-ui/static/js/runtime-main.5c7e3df8.js"
+  },
+  {
+    "revision": "49174c736a9c871baaeb2db6d5372eee",
+    "url": "/serum-dex-ui/static/media/logo.49174c73.svg"
+  }
+]);
